9747eb4d175eb75c5d1177378e7669e4630cae8d
Alex <chaza.mckay@me.com>
2021-12-21 08:57:35 +1030

    Adding readme file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# password-generator
+# Week3-Homework
+
+# Password Generator  
+
+## Description 
+
+This is my Portfolio website with a navigation bar in the top right corner with a section about me and a section for my work.
+Also has a footer at the bottom of the page with links to some important places.
+I will be constanly adding to this with my most recent work.
+I will also be trying to keep my resume, linkinin and github all up to date.
+
+
+## Screenshot 
+
+![code refacter web page](./assets/images/generate-password-webpage.png)
+
+## Resources 
+
+**https://mckayjalex.github.io/portfolio-am/**
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+// This function generates a random password
 function generatePassword() {
 
     // Global variable declaration
@@ -21,41 +21,41 @@ function generatePassword() {
     let finalPassword = "";
     
     // Prompts user for desired length of password and will alert / ask again if its not in between 8 - 128
-    let passwordLength = window.prompt("How many characters would you like?\n(8-128)");
-    while (passwordLength < 8 || passwordLength > 128 ){
+    let passwordLength = window.prompt("How many characters would you like?\n(8-128)", "Enter Number");
+    while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
         if (!passwordLength) {
         finalPassword = "Password creation aborted!";
         return finalPassword;
         }
-        window.alert("Please enter a number between 8 - 128!");
+        window.alert("Please enter a NUMBER between 8 - 128!");
         passwordLength = window.prompt("How many characters would you like?\n(8-128)");   
     }
     do {
         if (passwordLength) {
-        // If user would like to use uppercase then it will add the uppercase string to the desiredCharacters variable
-        let confirmUppercase = window.confirm("Would you like to include uppercase characters?");
-        if (confirmUppercase) {
-            desiredCharacters = uppercase.concat(desiredCharacters);
-        }
-        // If user would like to use special characters then it will add the chracters string to the desiredCharacters variable
-        let confirmCharacters = window.confirm("Would you like to include special characters/symbols?");
-        if (confirmCharacters) {
-            desiredCharacters = characters.concat(desiredCharacters);
-        }
-        // If user would like to use lowercase then it will add the lowercase string to the desiredCharacters variable
-        let confirmLowercase = window.confirm("Would you like to include lowercase characters?");
-        if (confirmLowercase) {
-            desiredCharacters = lowercase.concat(desiredCharacters);
-        }
-        // If user would like to use numbers then it with add the numbers variable to the desiredCharacters variable
-        let confirmNumbers = window.confirm("Would you like to include numbers?");
-        if (confirmNumbers) {
-            desiredCharacters = numbers.concat(desiredCharacters);
-        }
-        if (desiredCharacters.length < 1) {
-            window.alert("You must select atleast 1 type of character!");
+            // If user would like to use uppercase then it will add the uppercase string to the desiredCharacters variable
+            let confirmUppercase = window.confirm("Would you like to include uppercase characters?");
+            if (confirmUppercase) {
+                desiredCharacters = uppercase.concat(desiredCharacters);
+            }
+            // If user would like to use special characters then it will add the chracters string to the desiredCharacters variable
+            let confirmCharacters = window.confirm("Would you like to include special characters/symbols?");
+            if (confirmCharacters) {
+                desiredCharacters = characters.concat(desiredCharacters);
+            }
+            // If user would like to use lowercase then it will add the lowercase string to the desiredCharacters variable
+            let confirmLowercase = window.confirm("Would you like to include lowercase characters?");
+            if (confirmLowercase) {
+                desiredCharacters = lowercase.concat(desiredCharacters);
+            }
+            // If user would like to use numbers then it with add the numbers variable to the desiredCharacters variable
+            let confirmNumbers = window.confirm("Would you like to include numbers?");
+            if (confirmNumbers) {
+                desiredCharacters = numbers.concat(desiredCharacters);
+            }
+            if (desiredCharacters.length < 1) {
+                window.alert("You must select atleast 1 type of character!");
+            } 
         } 
-    } 
     } while (desiredCharacters < 1);
     
     // This will grab a random character out of the desiredCharacters variable and add it to finalPassword until it is the same length as passwordLength
